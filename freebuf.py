0d3908a9b8da1739739720de36fd4e283a061b76
wxyin <wenxu.yin@protonmail.com>
2017-10-10 16:03:13 +0800

    add multiple pages analysis support

--- freebuf.py
+++ freebuf.py
@@ -7,15 +7,28 @@ import logging
 import urllib
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+page_count = 42
+base_url = 'http://www.freebuf.com/vuls/'
 
-url = 'http://www.freebuf.com/vuls/'
-webpage = urllib.urlopen('http://www.freebuf.com/vuls/').read()
-soup = BeautifulSoup(webpage, "html5lib")
-news_list = soup.find_all('div', {'class': 'news-info'})
-news_title_list = []
-for news in news_list:
-    news_title = news.a.get('title')
-    print news_title
-    news_title_list.append(news_title)
+news_title = []
+def page_news_title(page_number):
+    page_url = base_url + "page/" + str(page_number)
+    print page_url
+    webpage = urllib.urlopen(page_url).read()
+    soup = BeautifulSoup(webpage, "html5lib")
+    news_list = soup.find_all('div', {'class': 'news-info'})
+    news_title_list = []
+    for news in news_list:
+        news_title = news.a.get('title')
+        print news_title
+        news_title_list.append(news_title)
+    return news_title_list
+
+for page_number in range(0, page_count):
+    page_number = page_number + 1
+    print "This is page " + str(page_number)
+    news_title = news_title + page_news_title(page_number)
+    
+
 
 
